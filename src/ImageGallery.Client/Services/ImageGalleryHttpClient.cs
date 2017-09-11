c5a6fdc2d6a79743bec018010b4ee1948beb6044
degoeym <degoeym@gmail.com>
2017-09-11 16:42:34 -0400

    Finished with module nine

--- src/ImageGallery.Client/Services/ImageGalleryHttpClient.cs
+++ src/ImageGallery.Client/Services/ImageGalleryHttpClient.cs
@@ -5,6 +5,8 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using IdentityModel.Client;
+using System.Globalization;
 
 namespace ImageGallery.Client.Services
 {
@@ -23,8 +25,26 @@ namespace ImageGallery.Client.Services
             var accessToken = string.Empty;
             var currentContext = _httpContextAccessor.HttpContext;
 
-            accessToken = await currentContext.Authentication.GetTokenAsync(
-                OpenIdConnectParameterNames.AccessToken);
+            //accessToken = await currentContext.Authentication.GetTokenAsync(
+            //    OpenIdConnectParameterNames.AccessToken);
+
+            // Should we renew access and refresh tokens?
+            // get expires_at value
+            var expires_at = await currentContext.Authentication.GetTokenAsync("expires_at");
+
+            // compare - make sure to use the exact date formats for comparison
+            // (UTC, in this case)
+            if (string.IsNullOrWhiteSpace(expires_at) 
+                || ((DateTime.Parse(expires_at).AddSeconds(-60)).ToUniversalTime() < DateTime.UtcNow))
+            {
+                accessToken = await RenewTokens();
+            }
+            else
+            {
+                // get access token
+                accessToken = await currentContext.Authentication
+                    .GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+            }
 
             if (!string.IsNullOrWhiteSpace(accessToken))
             {
@@ -37,7 +57,59 @@ namespace ImageGallery.Client.Services
                 new MediaTypeWithQualityHeaderValue("application/json"));
 
             return _httpClient;
-        }        
+        }
+        
+        private async Task<string> RenewTokens()
+        {
+            var currentContext = _httpContextAccessor.HttpContext;
+
+            // get the metadata
+            var discoveryClient = new DiscoveryClient("https://localhost:44360/");
+            var metadataResponse = await discoveryClient.GetAsync();
+
+            // create a new token client to get new tokens
+            var tokenClient = new TokenClient(metadataResponse.TokenEndpoint, 
+                "imagegalleryclient", "secret");
+
+            // get the saved refresh token
+            var currentRefreshToken = await currentContext.Authentication
+                .GetTokenAsync(OpenIdConnectParameterNames.RefreshToken);
+
+            var tokenResult = await tokenClient.RequestRefreshTokenAsync(currentRefreshToken);
+
+            if (!tokenResult.IsError)
+            {
+                // Svae the tokens
+
+                // get auth info
+                var authenticateInfo = await currentContext.Authentication
+                    .GetAuthenticateInfoAsync("Cookies");
+
+                // create a new value for expires_at, and save it
+                var expiresAt = DateTime.UtcNow + TimeSpan.FromSeconds(tokenResult.ExpiresIn);
+                authenticateInfo.Properties.UpdateTokenValue("expires_at", 
+                    expiresAt.ToString("o", CultureInfo.InvariantCulture));
+
+                authenticateInfo.Properties.UpdateTokenValue(
+                    OpenIdConnectParameterNames.AccessToken,
+                    tokenResult.AccessToken);
+                authenticateInfo.Properties.UpdateTokenValue(
+                    OpenIdConnectParameterNames.RefreshToken,
+                    tokenResult.RefreshToken);
+
+                // we're signing in again with the new values
+                await currentContext.Authentication.SignInAsync("Cookies",
+                    authenticateInfo.Principal, authenticateInfo.Properties);
+
+                // return the new access token
+                return tokenResult.AccessToken;
+            }
+            else
+            {
+                throw new Exception("Problem encountered while refreshing tokens.",
+                    tokenResult.Exception);
+            }
+        }
     }
 }
 
